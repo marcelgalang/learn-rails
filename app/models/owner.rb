48597fbeb3127d8613041d9c3172b08f79bd57b8
marcelgalang <marcelgalang@gmail.com>
2015-04-28 12:37:33 -0400

    dynamic home page

---
+++ app/models/owner.rb
@@ -0,0 +1,22 @@
+class Owner
+
+	def name
+		name = 'Sophie So So'
+	end
+
+	def birthdate
+		birthdate = Date.new(2014, 7, 3)
+	end
+
+	def countdown
+		today = Date.today
+		birthday = Date.new(today.year, birthdate.month, birthdate.day)
+		if birthday > today
+			countdown = (birthday - today).to_i
+		else
+			countdown = (birthday.next_year - today).to_i
+		end
+	end
+	
+		
+	end
