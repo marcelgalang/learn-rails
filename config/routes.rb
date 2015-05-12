d48aed78659523349374137648fcb1de84f9374b
marcelgalang <marcelgalang@gmail.com>
2015-05-12 10:50:28 -0400

    front-end framework

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
-LearnRails::Application.routes.draw do
-root to: 'visitors#new'
+Rails.application.routes.draw do
+  root to: 'visitors#new'
 end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
